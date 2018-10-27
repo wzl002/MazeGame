141bb8a920fc11c97218359cc66c475d585b038d
wesley <107821628@qq.com>
2018-10-27 15:00:58 -0700

    Make Enemy Moveable with simple AI.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool walk;
+
+
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        animator.SetFloat("Walk", walk ? 1 : 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Enemy OnTriggerEnter " + other.tag);
+        
+        if (other.tag == "Wall")
+        {
+            // Debug.Log("Enemy Rotate");
+            int d = Mathf.CeilToInt(Random.Range(0.1f, 3)); // random 1,2,3
+            transform.Rotate(0, 90 * d, 0);
+        }
+
+    }
+
 }
